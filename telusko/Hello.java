13c21c7edeb24be5ab0d5b7321cb0fdbefecc026
johnson <ashirwadayirumala@gmail.com>
2024-08-27 00:34:10 +0530

    five

--- telusko/Hello.java
+++ telusko/Hello.java
@@ -1,30 +1,19 @@
 // import java.io.*;
 import java.lang.String;
-
 class A{
-
-    public A(){
-        System.out.println("in A");
-    }
-      public A(int n){
-        System.out.println("in A int");
-    }
-
-}class B extends A{
-    public B(){
-      
-
-        System.out.println("in B");
-    }
-    public B(int n){
-        this();//will constructor of same class
-        System.out.println("in B int");
+    public void show(){
+        System.out.println("in A show");
     }
 }
-
+class B extends A{
+     public void show(){//override this method of class
+        System.out.println("in B show");
+    }
+}
 public class Hello{
     public static void main(String a[]){
-      B b = new B(5);
+        B ob = new B();
+        ob.show();
     }
 
 }
