32b268b763c8d769875374387dc2d0d4137aef3d
johnson <ashirwadayirumala@gmail.com>
2024-08-30 23:16:33 +0530

    seven

--- telusko/Hello.java
+++ telusko/Hello.java
@@ -1,25 +1,8 @@
 
 
-final class Calc{
-    public void show(){
-        System.out.println("in Calc show");
-    }
-    public void add(int a, int b){
-        System.out.println(a+b);
-    }
-}
-class AdvCalc extends Calc{
-    public void r(){
-        System.out.println("bla bla");
-    }
-}
 public class Hello{
     public static void main(String a[]){
-       Calc obj = new Calc();
-       obj.show();
-       obj.add(4, 5);
-       
-    }
+   
 
 }
 
