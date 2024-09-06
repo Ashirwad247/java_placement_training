bc8ac11ad5c8962bbdcaf4ea5791c375a656e874
johnson <ashirwadayirumala@gmail.com>
2024-09-06 22:58:56 +0530

    eleven

--- Dsa/one.java
+++ Dsa/one.java
@@ -1,32 +1,23 @@
-
+import java.io.*;
 class one{
-  //automorphic nubmer
-  //works for up to only two digit numbers n
-   public static void main(String []args){
-    int n = 36;
-    int sq = n*n;
-    int accum=0;
-    int count =0;
-    while(sq!=0){
-        if(count==1){
-        int rem =sq%10;
-        sq/=10; 
-        accum=(rem*10)+accum;
-        System.out.println(accum);
-        if(accum==n) break;
-        }else{
-        int rem =sq%10;
-        sq/=10; 
-        accum=rem;
-        if(accum==n) break;
 
-        count++;
-        }
-      
-    
+   public static void main(String []args)throws IOException{
+    int arr[] = {1,2,3 ,4, 5};
+    int i = 0;
+    int j = arr.length-1;
+    while(i!=j){
+      int temp = arr[i];
+      arr[i] = arr[j];
+     arr[j]=temp;
+      i++;
+      j--;
     }
-    if(accum==n)System.out.println("Automorphic number");
-    
+    for(int n:arr){
+      System.out.print(n+" ");
+    }
    }
+
+
+   
 
 }
---
+++ Practice/p.java
@@ -0,0 +1,13 @@
+
+class Mobile{
+  
+}
+
+class p{
+
+    
+    public static void main(String []args){
+
+      //line 431
+    }
+}
