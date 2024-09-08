593a79182fbf5abb6a205664b229b1fe457fd8f0
johnson <ashirwadayirumala@gmail.com>
2024-09-08 23:51:22 +0530

    thirteen

--- Dsa/one.java
+++ Dsa/one.java
@@ -1,33 +1,38 @@
 import java.io.*;
 import java.util.*;
 class one{
-  //two-sum leetcode
-   public static void main(String []args){
-    int arr[] = {3,2, 4};//becomes 234
-    int[][] a = new int[arr.length][2];
-    for(int i=0;i<arr.length;i++){
-      a[i][0] = arr[i];
-      a[i][1] = i;
-    }
-    int t = 6;
-    Arrays.sort(a, (c, d)->Integer.compare(c[0], d[0]));
-    // System.out.println(arr);
-    int l = 0, r = arr.length-1;
-    // for(int n:arr){
-    //   System.out.println(n);
-    // }
-    while(l<r){
-      int sum = a[l][0]+a[r][0];
-      if(sum==t){ System.out.println(a[l][1]);
-        System.out.println(a[r][1]);
-        break;}
-      else if(sum<t) l++;
-      else r--;
+  static void printOrder(int []a, int n){
+    int temp;
+    for(int i =0;i<2;i++){
+      for(int j =0;j<n/2;j++ ){
+        if(a[j]>a[j+1]){
+          temp = a[j];
+          a[j] = a[j+1];
+          a[j+1] = temp;
+        }
+      }
+      for(int j  =n/2;j<n-1;j++){
+        if(a[j]<a[j+1]){
+          temp = a[j];
+          a[j] = a[j+1];
+          a[j+1] = temp;
+        }
+      }
 
     }
+
+    for(int s:a){
+      System.out.println(s);
+    }
+  }
   
+  public static void main(String args[]){
+   int arr[] = {3, 2, 4, 1, 5};
+   int n = arr.length;
+   printOrder(arr, n);
+  }
 
-    }
+
    
 
 
--- Practice/p.java
+++ Practice/p.java
@@ -1,35 +1,3 @@
-import java.io.*;
-import java.util.*;
-//user defined exceptiption
-
-
-class P extends Exception{
-
-     private static int bal[] = {1001, 1002, 1300, 999, 1500};//will not print 999 and 1500
-   P(){
-
-   }
-    P(String str){
-      super(str);
-    }
-    
-
-  
-      //line 431
-   
-    public static void main(String []args) {
-      
-      for(int i =0;i<5;i++){
-        try{
-        P pp = new P("Amoutn balance is less");
-        if(bal[i]<1000) throw pp;
-
-        System.out.println(bal[i]);
-
-        }catch(P pp){
-          pp.printStackTrace();
-        }
-      }
-     }
+class P{
     
 }
