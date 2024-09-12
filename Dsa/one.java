dbd49cd21e56e5c9d780b7dcd2680b61b80238a2
johnson <ashirwadayirumala@gmail.com>
2024-09-12 22:06:04 +0530

    thirt v3

--- Dsa/one.java
+++ Dsa/one.java
@@ -1,25 +1,39 @@
-import java.lang.*;
-import java.util.Arrays;
+import java.util.*;
 
-class one//using recursion
-{
-  //F(N)=(1)+(2*3)+(4*5*6)+...N.
-//F(4)=5167
-   
+public class one {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> res = new HashMap<>();
 
-    public static void main(String []args){
-      int n = 3;
-      System.out.println(term(1, 1, 3));
+        for (String s : strs) {
+            int[] count = new int[26];  // Array to store the count of each character
 
+            // Count the frequency of each character in the string
+            for (char c : s.toCharArray()) {
+                count[c - 'a']++;
+            }
+
+            // Convert the count array to a string key (or use Arrays.toString())
+            StringBuilder sb = new StringBuilder();
+            for (int i : count) {
+                // sb.append('#');  // Any delimiter to separate the counts
+                sb.append(i);
+            }
+            String key = sb.toString();
+            System.out.println(key);
+
+            // Group the strings with the same character counts
+            if (!res.containsKey(key)) {
+                res.put(key, new ArrayList<>());
+            }
+            res.get(key).add(s);
+        }
+
+        return new ArrayList<>(res.values());
     }
 
-    public static int term(int calculated, int current, int N){
-      if(current==N+1) return 0;
-      int i, cur=1;
-      for( i =calculated;i<calculated+current;i++){
-        cur*=i;
-      }
-      return cur+term(i, current+1, N);
+    public static void main(String[] args) {
+        one ga = new one();
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(ga.groupAnagrams(strs));
     }
-     
 }
