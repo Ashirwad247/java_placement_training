586d8596edd8cfd4acd11451bfe10cb3ac694779
johnson <ashirwadayirumala@gmail.com>
2024-09-13 22:10:59 +0530

    fourteen v3

--- Dsa/one.java
+++ Dsa/one.java
@@ -5,18 +5,24 @@ public class one {
     
 
     public static void main(String[] args) {
-     int[] arr = {-2, -3, 4, -1, -2, 1, 5, -3};
-        int n = arr.length;
-        int max_sum = Integer.MIN_VALUE, curr_sum = 0;
-
-        for (int i = 0; i < n; i++) {
-          curr_sum+=arr[i];
-          if(max_sum<curr_sum) max_sum=curr_sum;
-          if(max_sum<0) curr_sum=0;
+      int x = 1000000045;
+     boolean negFlag=false;
+        if(x<0) {negFlag=true;x=-x;}
+        int num=0;
+        int prevNum=0;
+        while(x>0){
+            int r=x%10;
+            num=(num*10)+r;
+            if((num-r)/10!=prevNum){
+                
+                break;
+            }
+            System.out.println(num);
+            prevNum = num;
+            x=x/10;
         }
-
-        System.out.println(max_sum);
-
+        num=negFlag?-num:num;
+        System.out.println(num);
     }
     
     
